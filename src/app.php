4861fbb4ea9497d009436fbf413a27facdd51c71
Tim Rodger <tim.rodger@gmail.com>
2015-11-25 21:52:35 +0000

    Adds base template and css

--- src/app.php
+++ src/app.php
@@ -29,7 +29,7 @@ $app->get('/', function(Request $request) use ($app){
 
     $data = ['one' => 'https://github.com/timothy-r/rndr-twig'];
 
-    return $app['twig']->render('index.twig', [
+    return $app['twig']->render('index.html', [
         'repositories' => $data,
     ]);
 });
